f800f90baec5ca3347c0439cd22519e7d6db035b
Duy <duyanhvn1999@gmail.com>
2021-03-18 23:32:20 +0700

    Completed Header

--- source/frontend/src/components/commons/Header/Header.js
+++ source/frontend/src/components/commons/Header/Header.js
@@ -6,35 +6,57 @@ import {
   SearchInput,
   LogoWrapper,
   Icon,
+  IconWrapperRes,
   SearchButton,
   CartWrapper,
   DetailCardWrapper,
+  NavBarWrapper,
+  NavItem,
+  ListWrapper,
+  HeaderFixed,
+  FakeHeader,
 } from './Style';
 
 function Header() {
   return (
     <BWrapper>
-      <HeaderWrapper p={18} between="true">
-        <SearchWrapper>
-          <SearchInput placeholder="Search..." />
-          <SearchButton>
-            <i className="bx bx-search" />
-          </SearchButton>
-        </SearchWrapper>
-        <LogoWrapper>
-          <img
-            src="https://cdn.shopify.com/s/files/1/0359/5979/9852/t/2/assets/logo.png?v=1833458243843515159"
-            alt="logo"
-          />
-        </LogoWrapper>
-        <CartWrapper>
-          <Icon className="bx bxs-shopping-bag" />
-          <DetailCardWrapper>
-            <p>Shopping Cart</p>
-            <p>0 item - 0.00</p>
-          </DetailCardWrapper>
-        </CartWrapper>
-      </HeaderWrapper>
+      <HeaderFixed>
+        <HeaderWrapper p={18}>
+          <SearchWrapper>
+            <SearchInput placeholder="Search..." />
+            <SearchButton>
+              <i className="bx bx-search" />
+            </SearchButton>
+          </SearchWrapper>
+          <LogoWrapper>
+            <img
+              src="https://cdn.shopify.com/s/files/1/0359/5979/9852/t/2/assets/logo.png?v=1833458243843515159"
+              alt="logo"
+            />
+          </LogoWrapper>
+          <CartWrapper>
+            <Icon className="bx bxs-shopping-bag" />
+            <DetailCardWrapper>
+              <p>Shopping Cart</p>
+              <p>0 item - 0.00</p>
+            </DetailCardWrapper>
+            <IconWrapperRes>
+              <Icon className="bx bx-menu" />
+              <Icon className="bx bx-search" />
+            </IconWrapperRes>
+          </CartWrapper>
+        </HeaderWrapper>
+        <NavBarWrapper>
+          <ListWrapper p={18} center="true">
+            <NavItem>Home</NavItem>
+            <NavItem>Categories</NavItem>
+            <NavItem>Brands</NavItem>
+            <NavItem>Best Sellers</NavItem>
+            <NavItem>Pages</NavItem>
+          </ListWrapper>
+        </NavBarWrapper>
+      </HeaderFixed>
+      <FakeHeader>1</FakeHeader>
     </BWrapper>
   );
 }
--- source/frontend/src/components/commons/Header/Style.js
+++ source/frontend/src/components/commons/Header/Style.js
@@ -1,19 +1,37 @@
 import styled from 'styled-components';
 import { Button } from '../../Buttons/Buttons';
-import { BContainer } from '../../Layout/Base';
+import { BContainer, BFluidContainer, BWrapper } from '../../Layout/Base';
+
+export const HeaderFixed = styled(BWrapper)`
+  position: fixed;
+  background-color: white;
+  z-index: 1000;
+`;
 
 export const HeaderWrapper = styled(BContainer)`
-  height: 4.5rem;
+  height: 90px;
   align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  @media (max-width: 550px) {
+    flex-direction: column;
+    justify-content: center;
+    height: 115px;
+  }
 `;
 
 export const LogoWrapper = styled.div`
   cursor: pointer;
-  /* width: 15%; */
+  @media (max-width: 767px) {
+    margin-left: 15px;
+  }
 `;
 
 export const SearchWrapper = styled.div`
   display: flex;
+  @media (max-width: 767px) {
+    display: none;
+  }
 `;
 
 export const SearchButton = styled(Button)`
@@ -31,17 +49,20 @@ export const SearchInput = styled.input`
   background-color: #ebebeb;
   width: 15rem;
   @media (max-width: 992px) {
-    width: 8rem;
+    width: 10rem;
   }
 `;
 
 export const CartWrapper = styled.div`
   display: flex;
   align-items: center;
-  line-height: 1.5rem;
+  justify-content: center;
   color: #676767;
-  font-weight: bold;
-  font-size: small;
+`;
+
+export const Icon = styled.i`
+  font-size: 25px;
+  padding: 15px;
   transition: 0.3s;
   cursor: pointer;
   &:hover {
@@ -49,16 +70,52 @@ export const CartWrapper = styled.div`
   }
 `;
 
-export const Icon = styled.i`
-  font-size: 1.5rem;
-  margin-right: 15px;
+export const DetailCardWrapper = styled.div`
+  line-height: 1.5rem;
+  font-weight: bold;
+  font-size: small;
+  transition: 0.3s;
+  cursor: pointer;
+  &:hover {
+    color: #d14031;
+  }
   @media (max-width: 992px) {
-    margin-right: 0;
+    display: none;
   }
 `;
 
-export const DetailCardWrapper = styled.div`
-  @media (max-width: 992px) {
+export const IconWrapperRes = styled.div`
+  display: none;
+  @media (max-width: 767px) {
+    display: block;
+  }
+`;
+
+export const NavBarWrapper = styled(BWrapper)`
+  background-color: #d14031;
+  @media (max-width: 967px) {
     display: none;
+  }
+`;
+
+export const ListWrapper = styled(BContainer)`
+  display: flex;
+  align-items: center;
+`;
+
+export const NavItem = styled.a`
+  padding: 1.5rem;
+  color: white;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    color: black;
+  }
+`;
+
+export const FakeHeader = styled.div`
+  height: 90px;
+  @media (max-width: 550px) {
+    height: 115px;
   }
 `;
