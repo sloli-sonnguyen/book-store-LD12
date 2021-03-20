dd122cf4235a475f518c0b40e8024b0326cf57f2
kyanh180320 <kyanh180320@gmail.com>
2021-03-20 21:52:13 +0700

    test Widget

--- source/frontend/src/components/Base/Widget/Style.js
+++ source/frontend/src/components/Base/Widget/Style.js
@@ -7,5 +7,98 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
   max-width: 400px;
   width: 400px;
+  height: 1000px;
+`;
+export const Category = styled.h3`
+  border-bottom: 1px solid #d7cbcb;
+  height: 3rem;
+  width: 19rem;
+
+  padding-top: 1.2rem;
+  margin-top: 1rem;
+`;
+
+export const ProductCategory = styled.ul`
+  height: 200px;
+  margin-top: 1.5rem;
+  margin-bottom: 2rem;
+  /* background-color:pink; */
+`;
+export const ProductItem = styled.li`
+  height: 2.2rem;
+`;
+
+export const WidgetShopByPrice = styled.div`
+  max-width: 400px;
+  width: 400px;
+  height: 200px;
+  margin-bottom: 3rem;
+  /* background-color:pink; */
+`;
+
+export const ShopByPrice = styled.h3`
+  border-bottom: 1px solid #d7cbcb;
+  height: 3rem;
+  width: 19rem;
+  /* background-color:pink; */
+  padding-top: 1.2rem;
+`;
+
+export const ShopByFormat = styled(ShopByPrice)``;
+
+export const WidgetContentPrice = styled.ul`
+  max-width: 400px;
+  width: 400px;
+  height: 600px;
+  position: relative;
+  position: absolute;
+
+  transition: 1s;
+  background-color: pink;
+`;
+
+export const ProductPrice = styled.li`
+  height: 2.2rem;
+`;
+export const ButtonPrice = styled.button`
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+`;
+
+export const ButtonFormat = styled(ButtonPrice)``;
+export const WidgetContentFormat = styled.div`
+  max-width: 400px;
+  width: 400px;
   height: 400px;
+  position: relative;
+  position: absolute;
+  background-color: tomato;
+  transition: 1s;
+`;
+
+export const WidgetContainer = styled.div`
+  max-width: 400px;
+  width: 400px;
+  height: 600px;
+  background-color: lightblue;
+  position: relative;
+`;
+
+export const WidgetShopByFormat = styled(WidgetShopByPrice)`
+  background-color: yellow;
+  position: relative;
+  position: absolute;
+  background-color: tomato;
+  transition: 1s;
+  top: ${({ click }) => (click ? 0 : '37%')};
+`;
+
+export const ClearDisplay = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: green;
+  position: absolute;
+  transition: 1s;
+  /* top: ${({ click }) => (click ? '60%' : '100%')}; */
 `;
--- source/frontend/src/components/Base/Widget/Widget.js
+++ source/frontend/src/components/Base/Widget/Widget.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import { Wrapper, Container } from './Style';
+import React, { useState } from 'react';
+import {
+  Wrapper,
+  Container,
+  ProductCategory,
+  ProductItem,
+  Category,
+  WidgetShopByPrice,
+  ShopByPrice,
+  WidgetContentPrice,
+  ProductPrice,
+  ButtonPrice,
+  WidgetContentFormat,
+  WidgetContainer,
+  WidgetShopByFormat,
+  ShopByFormat,
+  ButtonFormat,
+} from './Style';
 
 function Widget() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <Wrapper>
-      <Container>Widget</Container>
+      <Container>
+        <Category>Category</Category>
+        <ProductCategory>
+          <ProductItem>History</ProductItem>
+          <ProductItem>Fiction</ProductItem>
+          <ProductItem>Business</ProductItem>
+          <ProductItem>Crime</ProductItem>
+          <ProductItem>Architechture</ProductItem>
+          <ProductItem>Poetry</ProductItem>
+        </ProductCategory>
+        <WidgetContainer>
+          <WidgetShopByPrice>
+            <h1>Container</h1>
+            <ShopByPrice>
+              <ButtonPrice onClick={handleClick}>
+                {click ? <i className="bx bxs-up-arrow" /> : <i className="bx bxs-down-arrow" />}
+              </ButtonPrice>
+              Shop By Price
+            </ShopByPrice>
+            <WidgetContentPrice>
+              {/* WidgetContentPrice sẽ bọc tất cả của WidgetContentFormat */}
+              <ProductPrice>$100 - $200</ProductPrice>
+              <ProductPrice>$200 - $300</ProductPrice>
+              <ProductPrice>$300 - $400</ProductPrice>
+              <ProductPrice>$400 - $500</ProductPrice>
+              <ProductPrice>$500 - $700</ProductPrice>
+
+              <WidgetShopByFormat click={click}>
+                <ShopByFormat>
+                  <ButtonFormat onClick={handleClick}>
+                    {click ? (
+                      <i className="bx bxs-up-arrow" />
+                    ) : (
+                      <i className="bx bxs-down-arrow" />
+                    )}
+                  </ButtonFormat>
+                  Shop By Format
+                </ShopByFormat>
+
+                <WidgetContentFormat>
+                  <ProductPrice>Paperback</ProductPrice>
+                  <ProductPrice>Hardcover</ProductPrice>
+                  <ProductPrice>Kindle Edition</ProductPrice>
+                  <ProductPrice>Large Print</ProductPrice>
+                  <ProductPrice>Audible Audiobook</ProductPrice>
+                </WidgetContentFormat>
+              </WidgetShopByFormat>
+            </WidgetContentPrice>
+          </WidgetShopByPrice>
+        </WidgetContainer>
+      </Container>
     </Wrapper>
   );
 }
