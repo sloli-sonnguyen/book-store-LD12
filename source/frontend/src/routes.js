8a3253fb0deccea0cc8a7b92d73d8ce8f66369b4
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2021-03-26 20:42:07 +0700

    add card

--- source/frontend/src/routes.js
+++ source/frontend/src/routes.js
@@ -5,6 +5,7 @@ const ProductsPage = React.lazy(() => import('./pages/ProductsPage'));
 const ContactPage = React.lazy(() => import('./pages/ContactPage'));
 const ProductDetailPage = React.lazy(() => import('./pages/ProductDetailPage'));
 const BlogsPage = React.lazy(() => import('./pages/BlogsPage'));
+const CartPage = React.lazy(() => import('./pages/CartPage'));
 
 const routes = [
   { id: 1, path: '/', exact: true, name: 'HomePage', component: HomePage },
@@ -12,6 +13,7 @@ const routes = [
   { id: 3, path: '/contact', name: 'ContactPage', component: ContactPage },
   { id: 4, path: '/products/:id', name: 'ProductDetailPage', component: ProductDetailPage },
   { id: 5, path: '/blogs', exact: true, name: 'BlogsPage', component: BlogsPage },
+  { id: 6, path: '/cart', exact: true, name: 'CartPage', component: CartPage },
 ];
 
 export default routes;
