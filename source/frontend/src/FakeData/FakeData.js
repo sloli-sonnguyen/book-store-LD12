3eb2e46aec83a9c4f3e9faa51d2373ca4bafa941
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2021-03-27 17:38:14 +0700

    test NewArrival component with datas

---
+++ source/frontend/src/FakeData/FakeData.js
@@ -0,0 +1,74 @@
+export const books = [
+  {
+    id: 1,
+    title: 'Donate A Book',
+    author: 'DONALD WILLIAM',
+    normalPrice: 700.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-1_grande.jpg?v=1613036885',
+  },
+  {
+    id: 2,
+    title: 'Endless Summer',
+    author: 'JOHN ERIK',
+    normalPrice: 800.0,
+    salePrice: 300.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-100_grande.jpg?v=1587115881',
+  },
+  {
+    id: 3,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-34_grande.jpg?v=1587116925',
+  },
+  {
+    id: 4,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-40_grande.jpg?v=1587119146',
+  },
+  {
+    id: 5,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-44_grande.jpg?v=1587117561',
+  },
+  {
+    id: 6,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-65_grande.jpg?v=1587118346',
+  },
+  {
+    id: 7,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-46_grande.jpg?v=1587118513',
+  },
+  {
+    id: 8,
+    title: 'Home Made Meals',
+    author: 'ALICE JAMES',
+    normalPrice: 300.0,
+    salePrice: 120.0,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-4_grande.jpg?v=1587113962',
+  },
+];
--- source/frontend/src/components/HomePage/NewArrivals/NewArrival.js
+++ source/frontend/src/components/HomePage/NewArrivals/NewArrival.js
@@ -3,142 +3,60 @@ import { Wrapper, Title, Menu } from './Style';
 import { CustomButton } from '../../Buttons/Buttons';
 import { BContainer } from '../../Layout/Base';
 import Card from '../../Base/Card/Card';
+import { books } from '../../../FakeData/FakeData';
+
+const category = [
+  { id: 1, content: 'All' },
+  { id: 2, content: 'Cook Book' },
+  { id: 3, content: 'History' },
+  { id: 4, content: 'Fantancy' },
+  { id: 5, content: 'Romance' },
+  { id: 6, content: 'Science' },
+];
+
+const isActive = 'All';
 
 function NewArrival() {
   return (
     <Wrapper>
       <Title>New Arrivals</Title>
       <Menu>
-        <CustomButton activeColor="#d14031" color="white" mg={4}>
-          All
-        </CustomButton>
-        <CustomButton
-          color="white"
-          hoverColor="#d14031"
-          textColor="black"
-          textHoverColor="white"
-          mg={4}
-        >
-          Card Book
-        </CustomButton>
-        <CustomButton
-          color="white"
-          hoverColor="#d14031"
-          textColor="black"
-          textHoverColor="white"
-          mg={4}
-        >
-          History
-        </CustomButton>
-        <CustomButton
-          color="white"
-          hoverColor="#d14031"
-          textColor="black"
-          textHoverColor="white"
-          mg={4}
-        >
-          Fantancy
-        </CustomButton>
-        <CustomButton
-          color="white"
-          hoverColor="#d14031"
-          textColor="black"
-          textHoverColor="white"
-          mg={4}
-        >
-          Romance
-        </CustomButton>
-        <CustomButton
-          color="white"
-          hoverColor="#d14031"
-          textColor="black"
-          textHoverColor="white"
-          mg={4}
-        >
-          Science
-        </CustomButton>
+        {category.map((item) => {
+          if (item.content === isActive) {
+            return (
+              <CustomButton key={item.id} activeColor="#d14031" color="white" mg={4}>
+                {item.content}
+              </CustomButton>
+            );
+          }
+          return (
+            <CustomButton
+              key={item.id}
+              color="white"
+              hoverColor="#d14031"
+              textColor="black"
+              textHoverColor="white"
+              mg={4}
+            >
+              {item.content}
+            </CustomButton>
+          );
+        })}
       </Menu>
       <BContainer p={10} between="true" wrap="true">
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-101_grande.jpg?v=1587115881"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-16_grande.jpg?v=1587117190"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-46_grande.jpg?v=1587118513"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-65_grande.jpg?v=1587118346"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-4_grande.jpg?v=1587113962"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-71_grande.jpg?v=1587117773"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-102_grande.jpg?v=1587120877"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
-        <Card
-          xs={12}
-          sm={6}
-          md={3}
-          src="https://cdn.shopify.com/s/files/1/0359/5979/9852/products/shop-new-30_grande.jpg?v=1587116432"
-          subTitle="DONALD WILLIAM"
-          title="Donate A Book"
-          text1="$120.00"
-          text2="$700.00"
-        />
+        {books.map((item) => (
+          <Card
+            xs={12}
+            sm={6}
+            md={3}
+            src={item.imageUrl}
+            subTitle={item.author}
+            title={item.title}
+            text1={`${item.salePrice}$`}
+            text2={`${item.normalPrice}$`}
+            id={item.id}
+          />
+        ))}
       </BContainer>
     </Wrapper>
   );
