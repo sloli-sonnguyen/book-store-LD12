3b66f67f79e8699ee5926b0537aa15a540a62cb0
Duy <duyanhvn1999@gmail.com>
2021-03-31 23:04:33 +0700

    Complete Checkout

--- source/frontend/src/components/Checkout/CheckoutMain/CheckoutMain.js
+++ source/frontend/src/components/Checkout/CheckoutMain/CheckoutMain.js
@@ -1,8 +1,164 @@
-import React from 'react';
-import { Wrapper } from './Style';
+import React, { useState } from 'react';
+import {
+  Wrapper,
+  Container,
+  Logo,
+  Title,
+  Input,
+  SmallInput,
+  SmallInputWrapper,
+  CheckboxWrapper,
+  Checkbox,
+  CheckboxLabel,
+  InputWrapper,
+  Select,
+  ConfirmWrapper,
+  Button,
+  ReturnTitle,
+  Footer,
+  ReturnIcon,
+  LogoContainer,
+  OrdersContainer,
+  OrdersSummaryTitleWrapper,
+  OrdersSummaryWrapper,
+  Icon,
+  Dropdown,
+} from './Style';
+
+import {
+  ProductWrapper,
+  ProductList,
+  ImageTextWrapper,
+  ProductImageWrapper,
+  BookImage,
+  Badge,
+  ProductInfoWrapper,
+  TitleText,
+  MoreInfo,
+  PriceText,
+  TotalItemWrapper,
+  TotalTitle,
+  TotalList,
+  TotalWrapper,
+  TotalPrice,
+  SmallUSD,
+} from '../Orders/Style';
+
+import { countries, productCart } from '../../../FakeData/FakeData';
 
 function CheckoutMain() {
-  return <Wrapper>CheckoutMain</Wrapper>;
+  const [shouldOrders, setShouldOrders] = useState(false);
+  const handleClick = () => {
+    setShouldOrders(!shouldOrders);
+  };
+
+  const quantity = productCart.length;
+  const calcHeight = () => {
+    const height = 12 + quantity * 6;
+    return height;
+  };
+
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <Logo src="https://cdn.shopify.com/s/files/1/0359/5979/9852/t/2/assets/logo.png?3626" />
+      </LogoContainer>
+      <OrdersContainer onClick={handleClick}>
+        <OrdersSummaryWrapper>
+          <OrdersSummaryTitleWrapper>
+            <Icon className="bx bxs-cart" />
+            Show order summary
+            {shouldOrders ? (
+              <Icon className="bx bx-chevron-down" />
+            ) : (
+              <Icon className="bx bx-chevron-up" />
+            )}
+          </OrdersSummaryTitleWrapper>
+          <TotalPrice>$940.00</TotalPrice>
+        </OrdersSummaryWrapper>
+        <Dropdown height={calcHeight()} shouldOrders={shouldOrders}>
+          <ProductList>
+            {productCart.map((product) => (
+              <ProductWrapper key={product.id}>
+                <ImageTextWrapper>
+                  <ProductImageWrapper>
+                    <BookImage src={product.img} alt="book" />
+                    <Badge>{product.quantity}</Badge>
+                  </ProductImageWrapper>
+                  <ProductInfoWrapper>
+                    <TitleText>{product.title}</TitleText>
+                    <MoreInfo>
+                      {product.type} / {product.language}
+                    </MoreInfo>
+                  </ProductInfoWrapper>
+                </ImageTextWrapper>
+                <PriceText>${product.price}.00</PriceText>
+              </ProductWrapper>
+            ))}
+          </ProductList>
+          <TotalList>
+            <TotalItemWrapper>
+              <TotalTitle>Subtotal</TotalTitle>
+              <PriceText>$920.00</PriceText>
+            </TotalItemWrapper>
+            <TotalItemWrapper>
+              <TotalTitle>Shipping</TotalTitle>
+              <PriceText>$20.64</PriceText>
+            </TotalItemWrapper>
+          </TotalList>
+          <TotalWrapper>
+            <TotalTitle>Total</TotalTitle>
+            <TotalPrice>
+              <SmallUSD>USD</SmallUSD>
+              $940.00
+            </TotalPrice>
+          </TotalWrapper>
+        </Dropdown>
+      </OrdersContainer>
+      <Container>
+        <Title>Contact information</Title>
+        <InputWrapper>
+          <Input placeholder="Email or mobile phone number" type="email" />
+        </InputWrapper>
+        <CheckboxWrapper>
+          <Checkbox type="checkbox" id="update" />
+          <CheckboxLabel htmlFor="update">
+            Keep me up to date on news and exclusive offers
+          </CheckboxLabel>
+        </CheckboxWrapper>
+        <Title>Shipping address</Title>
+        <SmallInputWrapper>
+          <SmallInput placeholder="First name (optional)" name="fname" />
+          <SmallInput placeholder="Last name" name="lname" />
+        </SmallInputWrapper>
+        <Input placeholder="Address" name="address" />
+        <Input placeholder="Apartment, suite, etc. (optional)" name="address" />
+        <Input placeholder="City" name="address" />
+        <SmallInputWrapper>
+          <Select placeholder="Country">
+            {countries.map((country) => (
+              <option key={country.id} value={country.value}>
+                {country.value}
+              </option>
+            ))}
+          </Select>
+          <SmallInput placeholder="Postal code" name="postcode" />
+        </SmallInputWrapper>
+        <CheckboxWrapper>
+          <Checkbox type="checkbox" id="save" />
+          <CheckboxLabel htmlFor="save">Save this information for next time</CheckboxLabel>
+        </CheckboxWrapper>
+        <ConfirmWrapper>
+          <Button>Contiune to shipping</Button>
+          <ReturnTitle>
+            <ReturnIcon className="bx bxs-chevron-left" />
+            Return to cart
+          </ReturnTitle>
+        </ConfirmWrapper>
+        <Footer>All rights reserved Bookly-theme</Footer>
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default CheckoutMain;
--- source/frontend/src/components/Checkout/Orders/Style.js
+++ source/frontend/src/components/Checkout/Orders/Style.js
@@ -1,3 +1,102 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 45%;
+  min-height: 100vh;
+  background-color: #fafafa;
+  @media (max-width: 1100px) {
+    display: none;
+  }
+  box-shadow: 1px 0 0 #e1e1e1 inset;
+`;
+
+export const Container = styled.div`
+  padding: 4rem;
+  padding-left: 2.5rem;
+  padding-right: 30%;
+`;
+
+export const ProductList = styled.div`
+  padding-bottom: 1.5rem;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+export const ProductWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.9rem;
+  align-items: center;
+  margin-top: 15px;
+  position: relative;
+`;
+
+export const ProductInfoWrapper = styled.div`
+  line-height: 1.3rem;
+`;
+
+export const ProductImageWrapper = styled.div`
+  margin-right: 15px;
+  padding: 0 10px;
+  height: 4.5rem;
+  border: 1px solid #e1e1e1;
+  border-radius: 5px;
+  position: relative;
+`;
+
+export const BookImage = styled.img`
+  height: 100%;
+`;
+
+export const Badge = styled.span`
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  padding: 5px 10px;
+  border-radius: 50%;
+  background-color: rgba(114, 114, 114, 0.9);
+  color: white;
+`;
+
+export const TitleText = styled.p``;
+
+export const PriceText = styled.p``;
+
+export const MoreInfo = styled.p`
+  color: #a7b3ab;
+  font-size: 0.85rem;
+`;
+
+export const ImageTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 80%;
+`;
+
+export const TotalList = styled.div`
+  padding: 0.75rem 0;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+export const TotalItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0.75rem 0;
+`;
+export const TotalTitle = styled.p`
+  color: #737373;
+`;
+
+export const TotalWrapper = styled(TotalItemWrapper)`
+  padding: 0.75rem 0;
+`;
+
+export const SmallUSD = styled.span`
+  font-size: small;
+  margin-right: 10px;
+`;
+
+export const TotalPrice = styled.p`
+  font-size: large;
+  display: flex;
+  align-items: center;
+`;
