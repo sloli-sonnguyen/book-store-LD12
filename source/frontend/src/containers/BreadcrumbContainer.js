cdac19d51c6f7f58742cfcb1dcec379a7b55a08d
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2021-04-10 20:56:35 +0700

    remove container folder

--- source/frontend/src/containers/BreadcrumbContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Breadcrumb from '../components/Base/Breadcrumb/Breadcrumb';
-
-function BreadcrumbContainer() {
-  return <Breadcrumb />;
-}
-
-export default BreadcrumbContainer;
